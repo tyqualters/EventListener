1321e080a4a19d3e25c118d9ebd418938443ed76
Ty Qualters <12106131+tyqualters@users.noreply.github.com>
2021-10-12 04:51:03 -0500

    Adding templated arguments

--- eventlistener.hpp
+++ eventlistener.hpp
@@ -2,7 +2,7 @@
  * @file eventlistener.hpp
  * @author Ty Qualters (contact@tyqualters.com)
  * @brief A simple multi-threaded event listener in C++.
- * @version 0.2
+ * @version 0.3
  * @date 2021-10-11
  * 
  * @copyright Copyright (c) 2021
@@ -10,19 +10,29 @@
  */
 
 #pragma once
-
 #include <vector>
 #include <mutex>
 #include <functional>
 #include <algorithm>
 #include <memory>
 
+#ifdef __DEBUG
+#include <iostream>
+void EventListenerLog(const char* text)
+{
+    std::cout << text << std::endl;
+}
+void EventListenerError(const char* text)
+{
+    std::cerr << text << std::endl;
+}
+#else
+void EventListenerLog(const char* text) {}
+void EventListenerError(const char* text) {}
+#endif
+
 namespace EventListener
 {
-    /**
-     * @brief The object passed (event) => event's associated function.
-     * 
-     */
     struct SEvent
     {
         int id;
@@ -30,79 +40,60 @@ namespace EventListener
         std::string name;
     };
 
-    /**
-     * @brief Typedef for the EventFunction to make life easier.
-     * 
-     */
-    typedef std::function<void(SEvent)> EventFunction;
+    template <typename... arguments>
+    using EventFunction = std::function<void(SEvent, arguments...)>;
 
-    /**
-     * @brief The object stored for the event listener.
-     * 
-     */
     struct SListener
     {
         int id;
         void *address;
         const char *name;
-        EventFunction *fn;
+        void **fn;
     };
 
-    /**
-     * @brief The list of events to listen for.
-     * 
-     */
     std::vector<SListener> g_events{};
-    /**
-     * @brief The mutex for events.
-     * 
-     */
     std::mutex g_events_mutex;
 
-    /**
-     * @brief The function to call an event.
-     * 
-     * @param objAddress The address of the assoc. object.
-     * @param eventName The name of the event to call.
-     */
-    void CallEvent(void *objAddress, const char *eventName)
+    template <typename... arguments>
+    void CallEvent(void *objAddress, const char *eventName, arguments... args)
     {
+        const std::lock_guard<std::mutex> lock(g_events_mutex);
+        EventListenerLog("Calling listener event.");
         for (auto &event : g_events)
         {
             if (event.address == objAddress && eventName)
-                (*event.fn)(SEvent{event.id, (uintptr_t)objAddress, std::string(eventName)});
+                try
+                {
+                    (*(EventFunction<arguments...> *)event.fn)(SEvent{event.id, (uintptr_t)objAddress, std::string(eventName)}, args...);
+                    EventListenerLog("Successfully called listener event.");
+                } catch (std::exception &e) {
+                    EventListenerError("WARNING: Listener event threw an exception.");
+                }
         }
     }
 
-    /**
-     * @brief Create an Event Listener.
-     * 
-     * @param objAddress The address to the associated object. If global, set the value to nullptr.
-     * @param eventName The name of the event.
-     * @param pfn A pointer to a function that will accept the SEvent argument.
-     */
-    void CreateEventListener(void *objAddress, const char *eventName, EventFunction *pfn)
+    template <typename... arguments>
+    void CreateEventListener(void *objAddress, const char *eventName, EventFunction<arguments...> *pfn)
     {
         static int id = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
-        g_events.push_back(SListener{id++, objAddress, eventName, pfn});
+        EventListenerLog("Creating listener.");
+        g_events.push_back(SListener{id++, objAddress, eventName, (void **)pfn});
+        EventListenerLog("Listener created.");
     }
 
-    /**
-     * @brief Delete an Event Listener.
-     * 
-     * @param id The ID of the Event Listener.
-     * @return int Count of how many Event Listeners were deleted.
-     */
     int DeleteEventListener(int id)
     {
         int count = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
+        EventListenerLog("Scanning listeners.");
         std::vector<SListener>::iterator it;
         it = std::remove_if(g_events.begin(), g_events.end(), [&count, &id](SListener &listener) -> bool
             {
+                EventListenerLog("Checking listener.");
                 if (listener.id == id)
                 {
+                    EventListenerLog("Deleting listener.");
                     ++count;
                     return true;
                 }
@@ -112,21 +103,18 @@ namespace EventListener
         return count;
     }
 
-    /**
-     * @brief Deletes any Event Listener associated with an address.
-     * 
-     * @param objAddress The address associated with the events to be deleted.
-     * @return int Count of how many Event Listeners were deleted.
-     */
     int DeleteEventListeners(void *objAddress)
     {
         int count = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
+        EventListenerLog("Scanning listeners.");
         std::vector<SListener>::iterator it;
         it = std::remove_if(g_events.begin(), g_events.end(), [&objAddress, &count](SListener &listener) -> bool
             {
+                EventListenerLog("Checking listener.");
                 if (listener.address == objAddress)
                 {
+                    EventListenerLog("Deleting listener.");
                     return true;
                     ++count;
                 }
@@ -136,21 +124,18 @@ namespace EventListener
         return count;
     }
 
-    /**
-     * @brief Deletes any Event Listener associated with an event name.
-     * 
-     * @param eventName The name for an event.
-     * @return int Count of how many Event Listeners were deleted.
-     */
     int DeleteEventListeners(const char *eventName)
     {
         int count = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
+        EventListenerLog("Scanning listeners.");
         std::vector<SListener>::iterator it;
         it = std::remove_if(g_events.begin(), g_events.end(), [&eventName, &count](SListener &listener) -> bool
             {
+                EventListenerLog("Checking listener.");
                 if (listener.name == eventName)
                 {
+                    EventListenerLog("Deleting listener.");
                     return true;
                     ++count;
                 }
@@ -160,46 +145,41 @@ namespace EventListener
         return count;
     }
 
-    /**
-     * @brief Push an event to all Event Listeners associated with an event name.
-     * 
-     * @param eventName The name for an event.
-     * @return int Count for how many events have been pushed.
-     */
-    int PushEvent(const char *eventName)
+    template <typename... Args>
+    int PushEvent(const char *eventName, Args... args)
     {
         int count = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
-        std::for_each(g_events.begin(), g_events.end(), [&count, &eventName](SListener &listener) -> void
+        EventListenerLog("Scanning listeners.");
+        for (SListener &listener : g_events)
+        {
+            EventListenerLog("Checking listener.");
+            if (listener.name == eventName)
             {
-                if (listener.name == eventName)
-                {
-                    CallEvent(listener.address, eventName);
-                    ++count;
-                }
-            });
+                EventListenerLog("Calling listener function.");
+                CallEvent(listener.address, eventName, std::forward<Args>(args)...);
+                ++count;
+            }
+        }
         return count;
     }
 
-    /**
-     * @brief Push an event to all Event Listeners associated with an address and event name.
-     * 
-     * @param objAddress The address associated with the events.
-     * @param eventName The name for an event.
-     * @return int Count for how many events have been pushed.
-     */
-    int PushEvent(void *objAddress, const char *eventName)
+    template <typename... Args>
+    int PushEvent(void *objAddress, const char *eventName, Args... args)
     {
         int count = 0;
         const std::lock_guard<std::mutex> lock(g_events_mutex);
-        std::for_each(g_events.begin(), g_events.end(), [&count, &objAddress, &eventName](SListener &listener) -> void
+        EventListenerLog("Scanning listeners.");
+        for (SListener &listener : g_events)
+        {
+            EventListenerLog("Checking listener.");
+            if (listener.address == objAddress && listener.name == eventName)
             {
-                if (listener.address == objAddress && listener.name == eventName)
-                {
-                    CallEvent(listener.address, eventName);
-                    ++count;
-                }
-            });
+                EventListenerLog("Calling listener function.");
+                CallEvent(listener.address, eventName, std::forward<Args>(args)...);
+                ++count;
+            }
+        }
         return count;
     }
 }
